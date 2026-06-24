efcfba4d92b9ea110c084cf0ce286f2dc250258e
Danishan Farookh <danishan089@gmail.com>
2026-06-24 18:53:14 +0530

    feat: Added the Horizontal scroll in the toolbar when nodes overflow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,26 @@ import { PipelineUI } from "./ui";
 import { SubmitButton } from "./submit";
 
 import styles from "./App.module.css";
+import { useRef } from "react";
 
 function App() {
+  const toolbarRef = useRef(null);
+
+  const handleWheel = (event) => {
+    if (toolbarRef.current) {
+      event.preventDefault();
+      toolbarRef.current.scrollLeft += event.deltaY;
+    }
+  };
+
   return (
     <div className={styles.app}>
       <div className={styles.topbar}>
-        <div className={styles.toolbarWrapper}>
+        <div
+          className={styles.toolbarWrapper}
+          ref={toolbarRef}
+          onWheel={handleWheel}
+        >
           <PipelineToolbar />
         </div>
 
